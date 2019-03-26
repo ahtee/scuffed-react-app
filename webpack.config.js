0d5e9ea84146f05493e4d42ca8944af561793708
212615385 <benjamin.otte@ge.com>
2019-03-26 15:25:44 -0400

    Bump react version, add production module configs

--- webpack.config.js
+++ webpack.config.js
@@ -27,8 +27,52 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'public/'),
     port: 8080,
-    publicPath: 'http://localhost:8080/dist/',
+    publicPath: 'http://localhost:3000/dist/',
     hotOnly: true
+  }
+};
+
+module.exports = {
+  entry: '.src/index.js',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: /node_modules/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] }
+      },
+      {
+        test: /\.(ts)$/,
+        include: /node_modules/,
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: ['style-loader'](/loaders/style-loader) },
+          {
+            loader: ['css-loader'](/loaders/css-loader),
+            options: {
+              modules: true
+            }
+          },
+          { loader: ['sass-loader'](/loaders/sass-loader) }
+        ]
+      }
+    ]
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+  resolve: { extensions: ['*', '.js', '.jsx'] },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public/'),
+    port: 8080,
+    publicPath: 'http://localhost:3000/dist/',
+    hotOnly: true
+  }
 };
