e67dfd9e2f90d0a50b5cc16403815f8c213e9c93
Ben Otte <benottedev@gmail.com>
2019-09-04 15:34:57 -0400

    adjusting script for error reporting

--- src/utils/cli.js
+++ src/utils/cli.js
@@ -7,16 +7,18 @@ const fs = require('fs');
 try {
   console.time(`Installing new-react-app boilerplate`);
   if (arg) {
+    // Create a directory for the application name with the first argument.
     fs.mkdir(`./${arg}`, err => {
       if (err) {
-        throw err;
+        throw new Error(err);
       }
     });
   }
 } catch (err) {
-  console.error(
-    `Error running script! Check if your folder name already exists, choose a different folder name, or check the help command (--help, -h) for more details`
-  );
+  if (err)
+    console.error(
+      `Error running script! Check if your folder name already exists, choose a different folder name, or check the help command (--help, -h) for more details`
+    );
   console.trace(err);
 }
 console.timeEnd(`Installing new-react-app boilerplate`);
